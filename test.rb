f31e131bba05168ee537fe8d3e2aa6886b609f7e
rawenfalls <zaqmond@yandex.ru>
2018-01-22 21:59:00 +0300

    v1.84

--- test.rb
+++ test.rb
@@ -1,8 +1,12 @@
 require "./checker.rb"
 begin
-  entered_words = ("привет мир на НА На юпитер-9 от об - - - ").strip.scan(/[A-Za-zА-Яа-я0-9\-_]+/)
+  entered_words = ("привет мир на НА На юпитер-9 от об - - - ").strip.scan(/[А-Яа-я][\wА-Яа-я\-_]+/)
   check_words(entered_words)
   if entered_words.size == 3
     puts "программма работает исправно"
+  else
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    puts "!!!программма работает НЕИСПРАВНО!!!"
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   end
 end
